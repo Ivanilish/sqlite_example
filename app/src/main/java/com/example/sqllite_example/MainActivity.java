2ac0259046476807cd75d5c2d2dc8df6a562f4b0
relms <relms@gmail.com>
2024-11-13 15:47:28 -0500

    dabate-base

--- app/src/main/java/com/example/sqllite_example/MainActivity.java
+++ app/src/main/java/com/example/sqllite_example/MainActivity.java
@@ -1,54 +1,50 @@
 package com.example.sqllite_example;
 
-
-public class MainActivity extends AppCompatActivity {
-
-package com.example.maestrodetalle;
-
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
-    public class DetailActivity extends AppCompatActivity {
 
-        private DataBaseHelper dbHelper;
-        private TextView detailTitle;
-        private TextView detailDescription;
+public class DetailActivity extends AppCompatActivity {
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_detail);
+    private DataBaseHelper dbHelper;
+    private TextView detailTitle;
+    private TextView detailDescription;
 
-            dbHelper = new DatabaseHelper(this);
-            detailTitle = findViewById(R.id.detailTitle);
-            detailDescription = findViewById(R.id.detailDescription);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-            int itemId = getIntent().getIntExtra("ITEM_ID", -1);
-            if (itemId != -1) {
-                loadItemDetails(itemId);
-            }
-        }
+        dbHelper = new DataBaseHelper(this);
+        detailTitle = findViewById(R.id.detailTitle);
+        detailDescription = findViewById(R.id.detailDescription);
 
-        private void loadItemDetails(int itemId) {
-            SQLiteDatabase db = dbHelper.getReadableDatabase();
-            String selection = DatabaseHelper.COLUMN_ID + " = ?";
-            String[] selectionArgs = { String.valueOf(itemId) };
-
-            Cursor cursor = db.query(DatabaseHelper.TABLE_ITEMS, null, selection, selectionArgs, null, null, null);
-
-            if (cursor != null && cursor.moveToFirst()) {
-                String title = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_TITLE));
-                String description = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COLUMN_DESCRIPTION));
-
-                detailTitle.setText(title);
-                detailDescription.setText(description);
-
-                cursor.close();
-            }
+        int itemId = getIntent().getIntExtra("ITEM_ID", -1);
+        if (itemId != -1) {
+            loadItemDetails(itemId);
         }
     }
 
+    private void loadItemDetails(int itemId) {
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        String selection = DataBaseHelper.COLUMN_ID + " = ?";
+        String[] selectionArgs = { String.valueOf(itemId) };
+
+        Cursor cursor = db.query(DataBaseHelper.TABLE_ITEMS, null, selection, selectionArgs, null, null, null);
+
+        if (cursor != null && cursor.moveToFirst()) {
+            String title = cursor.getString(cursor.getColumnIndex(DataBaseHelper.COLUMN_TITLE));
+            String description = cursor.getString(cursor.getColumnIndex(DataBaseHelper.COLUMN_DESCRIPTION));
+
+            detailTitle.setText(title);
+            detailDescription.setText(description);
+
+            cursor.close();
+        }
+    }
 }
+
+
--- app/src/main/java/com/example/sqllite_example/maestro.java
+++ app/src/main/java/com/example/sqllite_example/maestro.java
@@ -1,15 +1,14 @@
 package com.example.sqllite_example;
 
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import androidx.appcompat.app.AppCompatActivity;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
